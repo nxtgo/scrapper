93116d4ecf61d703dc6442df4067b12fe57f54fa
Esteban Gonzalez <estebangonza2005@gmail.com>
2025-10-08 18:32:51 -0500

    r

--- example/main.go
+++ example/main.go
@@ -9,6 +9,7 @@ import (
 func main() {
 	elements, err := scrapper.ScrapByURL("https://github.com/nxtgo", "h1[dir=\"auto\"]")
 	if err != nil {
+		panic("do something")
 	}
 
 	for _, element := range elements {
